13d9972a807f8c1ac1205421512720eee69788c3
manuelloverso <manuelloverso60@gmail.com>
2026-05-26 21:59:58 +0200

    first commit

---
+++ src/main/java/com/manuel/safehub_api/SafehubApiApplication.java
@@ -0,0 +1,13 @@
+package com.manuel.safehub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafehubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafehubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuel/safehub_api/SafehubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuel.safehub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafehubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
